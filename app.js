589268d6ad83634bb65efca546adfb15086df13c
Huu Thuan <huuthuanlt336@gmail.com>
2022-03-03 15:43:27 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+
 const setupContactRoutes = require("./app/routes/contact.routes");
+
+const { BadRequestError , errorHandler } = require("./app/errors");
 
 const app = express();
 
@@ -18,5 +21,16 @@ app.get("/", (req, res) => {
 });
 
 setupContactRoutes(app);
+
+// handle 404 response
+app.use((req, res, next) => {
+    //Code ở đây sẽ chạy khi không có route đucợ định nghĩa nào
+    //   khớp với yêu cầu. Gọi next() để chuyển sang middlware xử lý lỗi
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+    errorHandler.handleError(error, res);
+});
 
 module.exports = app;
